21c832058344b8909fe4369e63ee1512699b739d
Sanat Deshpande <sanatdeshpandejhu@gmail.com>
2018-02-21 13:42:08 -0500

    change to running script

--- a1/thread_test.py
+++ a1/thread_test.py
@@ -5,17 +5,17 @@ count = [1,2,4,8,16]
 os.system("make")
 
 print("*****serial testing******")
-os.system("./runloop ./filter_serial")
+os.system("./runloop.sh ./filter_serial")
 
 print("******parallel testing no speedup*****")
-os.system("./runloop ./filter_parallel")
+os.system("./runloop.sh ./filter_parallel")
 
 print("******parallel testing with speedup*****")
 for c in count:
-    os.system("./runloop ./filter_parallel " + str(c))
+    os.system("./runloop.sh ./filter_parallel " + str(c))
 
 print("******loop unrolling*****")
-os.system("./runloop ./filter_parallel_unrolling 8")
+os.system("./runloop.sh ./filter_parallel_unrolling 8")
 
 print("******dynamic scheduling*****")
-os.system("./runloop ./filter_parallel_dynamic 8")
+os.system("./runloop.sh ./filter_parallel_dynamic 8")
