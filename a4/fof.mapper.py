1c814ef9326b624a972a2d03d0cfc06a78f26303
Sanat Deshpande <sanatdeshpandejhu@gmail.com>
2018-04-09 23:30:41 -0400

    mapper works

--- a4/fof.mapper.py
+++ a4/fof.mapper.py
@@ -2,4 +2,24 @@
 
 import sys
 
-print(sys.stdin.read())
+#initialize map and list
+friend_map = {}
+friend_list = [i for i in sys.stdin.read().split("\n") if len(i) > 0]
+
+#build map
+for f in friend_list:
+    f_key = f.split(" ")[0] #gets key
+    friends = [i for i in f.split(" ")[1:]] #gets value list (friends)
+    if f_key in friend_map:
+        friend_map[f_key].append(friends)
+    else:
+        friend_map[f_key] = [friends]
+
+#outputs mapping
+for i in friend_map:
+    sys.stdout.write(i + "|")
+    for j in friend_map[i]:
+        for k in j:
+            sys.stdout.write(k + " ")
+        sys.stdout.write(", ")
+    sys.stdout.write("\n")
--- a4/transfer.py
+++ a4/transfer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python2.7
-
 import os
-
 
 os.system("scp -P 2222 ./* parallel@localhost:~/project/")
